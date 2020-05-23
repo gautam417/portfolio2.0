dc98a61f0d447befbccad0243f9c3bb6e81ed8e9
gautam417 <gautammehta@Gautams-MacBook.local>
2020-05-23 01:39:09 -0700

    Remove proxy

--- src/App.js
+++ src/App.js
@@ -25,13 +25,5 @@ class App extends Component {
     );
   }
 }
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('build'));
-  app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}app.listen(PORT);
 
 export default App;
