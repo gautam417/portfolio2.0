c417f5995ba85fef5a3cff7670527ad48566920c
gautam417 <gautammehta@Gautams-MacBook.local>
2020-05-23 02:00:12 -0700

    Add image logo

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/patagonia.png)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/Patagonia.png)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Gautam Mehta</a></h1>
               <span className="email"><i className="icon-mail"></i>gmehta1@uci.edu</span>
             </div>
